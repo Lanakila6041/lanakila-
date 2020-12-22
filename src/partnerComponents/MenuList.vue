f12957baff63fc944f9d42ec56267168531843f4
Daagii Daagii <dalantai@aggregator.mn>
2020-12-23 00:35:14 +0800

    daagii add work and add status

--- src/partnerComponents/MenuList.vue
+++ src/partnerComponents/MenuList.vue
@@ -39,10 +39,10 @@
 							</el-table-column>
 							<el-table-column prop="work_status" label="Төлөв" width="100">
 								<template slot-scope="scope">
-									<el-tag v-if="scope.row.work_status === 'TO DO'" size="mini" type="info">
-										{{scope.row.work_status}}</el-tag>
-									<el-tag v-if="scope.row.work_status === 'DONE'" size="mini" type="success">
-										{{scope.row.work_status}}</el-tag>
+									<el-button @click="changeType(scope.row)" v-if="scope.row.work_status === 'TO DO'" size="mini" type="info" plain>
+										{{scope.row.work_status}}</el-button>
+									<el-button v-if="scope.row.work_status === 'done'" size="mini" type="success" plain>
+										{{scope.row.work_status}}</el-button>
 								</template>
 							</el-table-column>
 						</el-table>
@@ -54,10 +54,43 @@
 			</el-row>
 		</div>
 		<el-dialog title="Төлөвлөгөөт ажил нэмэх" :visible.sync="dialogVisible" width="30%">
-			<span>Төлөвлөгөөт ажил нэмэх</span>
+			<el-form ref="form" :model="workForm" label-width="120px" size="mini" label-position="top">
+				<el-form-item label="Ажлын нэр">
+					<el-input v-model="workForm.work_name" placeholder="Та төлөвлөж буй ажлынхаа нэрийг оруулна уу"></el-input>
+				</el-form-item>
+				<el-form-item label="Ажлын тайлбар">
+					<el-input v-model="workForm.work_description" placeholder="Та төлөвлөж буй ажлынхаа тайлбарыг оруулна уу"></el-input>
+				</el-form-item>
+				<el-form-item label="Ажлыг гүйцэтгэх хугацаа">
+					<el-input-number v-model="workForm.work_time_point" :min="0" :max="10"></el-input-number>
+				</el-form-item>
+				<el-form-item label="Ажлыг гүйцэтгэх огноо">
+					<el-date-picker style="width:100%" v-model="workForm.work_do_time" type="date" value-format="yyyy-MM-dd" placeholder="Ажлыг гүйцэтгэх огноог сонгож өгнө үү"></el-date-picker>
+				</el-form-item>
+				<el-form-item label="Таны нийт ангилаас аль ангилалд багтах вэ?">
+					<el-select style="width:100%" v-model="workForm.work_category" placeholder="Ажилд хамаарах ангиллыг сонгоно уу">
+						<el-option
+						v-for="item in myCategories"
+						:key="item.value"
+						:label="item.label"
+						:value="item.value">
+						</el-option>
+					</el-select>
+				</el-form-item>
+				<el-form-item label="Энэхүү ажлын төрлийг сонгоно уу">
+					<el-select style="width:100%" v-model="workForm.work_type" placeholder="Энэхүү ажил нь ямар төрлийн ажил вэ?">
+						<el-option
+						v-for="item in myTypes"
+						:key="item.value"
+						:label="item.label"
+						:value="item.value">
+						</el-option>
+					</el-select>
+				</el-form-item>
+			</el-form>
 			<span slot="footer" class="dialog-footer">
-				<el-button @click="dialogVisible = false">Cancel</el-button>
-				<el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+				<el-button size="mini" @click="clearDialog()">Буцах</el-button>
+				<el-button type="primary" size="mini" @click="addNewWork()">Ажлыг нийт төлөвлөгөөнд нэмэх</el-button>
 			</span>
 		</el-dialog>
 	</div>
@@ -73,6 +106,41 @@ export default {
 	},
 	data () {
 		return {
+			workForm: {
+				work_do_time: '',
+				work_name: '',
+				work_description: '',
+				work_time_point: 0,
+				work_status: 'TO DO',
+				work_type: '',
+				work_category: '',
+				created_at: ''
+			},
+			myCategories: [{
+				value: 'Сүлжээ',
+				label: 'Сүлжээ'
+			}, {
+				value: 'Төсөл',
+				label: 'Төсөл'
+			}, {
+				value: 'Сүлжээний удирдлага',
+				label: 'Сүлжээний удирдлага'
+			}, {
+				value: 'Алгоритм',
+				label: 'Алгоритм'
+			}
+			],
+			myTypes: [{
+				value: 'high',
+				label: 'Хэцүү'
+			}, {
+				value: 'medium',
+				label: 'Дундаж'
+			}, {
+				value: 'easy',
+				label: 'Амархан'
+			}
+			],
 			ListData: [{
 				work_do_time: '2020-12-25',
 				work_name: 'Pizzahut web',
@@ -104,14 +172,69 @@ export default {
 				created_at: '2020-12-20'
 			}
 			],
-			dialogVisible: false
+			dialogVisible: false,
+			sendUpdateData: {}
 		}
 	},
 	methods: {
+		changeType (data) {
+			let payload = data
+			this.$confirm('Та энэхүү ажлыг хийж дууссан уу?', 'Анхаар', {
+				confirmButtonText: 'Тийм',
+				cancelButtonText: 'Үгүй',
+				type: 'warning'
+			}).then(() => {
+				axios.post('http://localhost:8000/api/workSearchIdUpdate', payload)
+					.then((res) => {
+						if (res.data.status === 200) {
+							this.$notify({
+								title: 'Амжилттай',
+								message: 'Энэхүү ажлын төлөв өөрчлөгдлөө',
+								type: 'success'
+							})
+							location.reload()
+						} else {
+							this.$notify({
+								title: 'Анхаар',
+								message: 'алдаа гарлаа',
+								type: 'error'
+							})
+						}
+					})
+			}).catch(() => {
+				this.$message({
+					type: 'info',
+					message: 'Delete canceled'
+				})
+			})
+		},
+		clearDialog () {
+			this.workForm = {}
+			this.dialogVisible = false
+		},
 		getWorkList () {
 			axios.get('http://localhost:8000/api/getAllWorks').then(resp => {
 				this.ListData = resp.data
 			})
+		},
+		addNewWork () {
+			axios.post('http://localhost:8000/api/addWorkNew', this.workForm)
+				.then((res) => {
+					if (res.data.status === 200) {
+						this.$notify({
+							title: 'Амжилттай',
+							message: 'Таны төлөвлөгөөт ажил амжилттай үүслээ',
+							type: 'success'
+						})
+						location.reload()
+					} else {
+						this.$notify({
+							title: 'Анхаар',
+							message: 'Бүртгэл үүсгэх явцад алдаа гарлаа',
+							type: 'error'
+						})
+					}
+				})
 		},
 		showAddWorkDialog () {
 			this.dialogVisible = true
